057f86966121bcc61586f388fd3e988c4277f62c
Pietro Grandi <pietro.grandi.3d@gmail.com>
2020-11-26 21:34:26 +0100

    Fetch is not available in Node

--- my_functions/drawNames.js
+++ my_functions/drawNames.js
@@ -1,3 +1,5 @@
+const request = require('request');
+
 exports.handler = async function(event, context) {
     // your server-side functionality
     const payload = JSON.parse(event.body)
@@ -11,22 +13,27 @@ exports.handler = async function(event, context) {
     const url = `https://http-api.d7networks.com/send?username=${USN}&password=${PWD}`
     const config = `&dlr-method=POST&dlr-url=https://4ba60af1.ngrok.io/receive&dlr=yes&dlr-level=3`
 
+    const options = {
+        method: 'POST',
+        headers: {},
+        formData: {}
+      };
+
     const promises = recipients.forEach(element => {
         const content = escape(`
             Ciao ${element.name}! Il budget e' ${budget}.
         `);
         const number = escape(element.number);
         const meta = `&from=${from}&content=${content}&to=${number}`
+        options.url = `${url}${config}${meta}`
 
-        fetch(`${url}${config}${meta}`, 
-            { method: "POST"}
-        )
+        request(options, function (error, response) {
+            if (error) {
+                return { statusCode: 500, body: e.message }
+            }
+            console.log(response.body);
+          });
     });
 
-    try {
-        await Promise.all(promises)
-    } catch(e) {
-        return { statusCode: 500, body: e.message }
-    }
-    return { statusCode: 200, body: "OK" }
-}
+    return { statusCode: 200, body: "OK" };
+};
