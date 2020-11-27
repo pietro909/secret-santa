99f0816696ee410d3ab3ba0a7cc8dc0f7f67e84b
Pietro Grandi <pietro.grandi.3d@gmail.com>
2020-11-27 13:30:50 +0100

    use Axios

--- my_functions/drawNames.js
+++ my_functions/drawNames.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const axios = require('axios').default;
 
 exports.handler = async function(event, context) {
     // your server-side functionality
@@ -8,37 +8,31 @@ exports.handler = async function(event, context) {
 
     const from = escape("Secret Santa")
 
-    const USN = process.env.SMS_NAME
-    const PWD = process.env.SMS_PASSWORD
-    const url = `https://http-api.d7networks.com/send?username=${USN}&password=${PWD}`
-    const config = `&dlr-method=POST&dlr-url=https://4ba60af1.ngrok.io/receive&dlr=yes&dlr-level=3`
+    const USN = process.env.SMS_NAME;
+    const PWD = process.env.SMS_PASSWORD;
+    const url = `https://http-api.d7networks.com/send?username=${USN}&password=${PWD}`;
+    const config = `&dlr-method=POST&dlr-url=https://4ba60af1.ngrok.io/receive&dlr=yes&dlr-level=3`;
+    
+    console.log(recipients);
 
-    const options = {
-        method: 'POST',
-        headers: {},
-        formData: {}
-      };
-
-      console.log(recipients);
-
-    recipients.forEach(element => {
+    const promises = recipients.map(element => {
         const content = escape(`Ciao ${element.name}! Il budget e' ${budget}.`.replace("&", ""));
         const number = escape(element.number);
-        const meta = `&from=${from}&content=${content}&to=${number}`
-        options.url = `${url}${config}${meta}`
+        const meta = `&from=${from}&content=${content}&to=${number}`;
 
         console.log(`sending to ${element.number} for ${element.name}`);
         console.log(options);
 
-        setTimeout(() => 
-            request(options, function (error, response) {
-                if (error) {
-                    return { statusCode: 500, body: e.message }
-                }
-                console.log(response.body);
-            })
-        , 1000);
+        return axios.post(`${url}${config}${meta}`);
     });
 
+    try {
+        const results = await Promise.all(promises);
+        console.log("DONE!", results);
+    } catch(e) {
+        console.error(e);
+        return { statusCode: 200, body: e.message };
+    }
     return { statusCode: 200, body: "OK" };
 };
+
